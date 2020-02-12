45d502cea8be5a5a5a5cac99a8f946eb2046056a
Lucia Cristiano <cristial@mcmaster.ca>
2020-02-12 15:10:08 -0500

    adding doxygen comments

--- src/endTurn.py
+++ src/endTurn.py
@@ -1,5 +1,14 @@
+## @file endTurn.py
+#  @author Lucia Cristiano
+#  @brief This program performs various functions neeeded to update the front end after a move is validated. 
+#  @date Feb.12,2020
 class endTurn:
 
+    ## @brief updates the back end version of the board with the valid word.
+    # @param1 an integer that represents the row of an the back end array.
+    # @param2 an integer that represents the column of the back end array.
+    # @param3 a string that represents the direction of the word placed on the board.
+    # @param4 a string that represents the word being placed on the board.
     def updateBackBoard(row, col, dir, word):
         if(dir.lower() = "right")
             countCol = col
@@ -11,7 +20,12 @@ class endTurn:
             for char in word:
                 backBoard[col][countRow] = char
                 countRow ++
-    
+    ## @brief updates the back end version of the board with the valid word.
+    # @param1 an integer that represents the row of an the back end array.
+    # @param2 an integer that represents the column of the back end array.
+    # @param3 a string that represent the direction of the word placed on the board.
+    # @param4 a string that represents the word being placed on the board.
+    # @returns a list of tuples representing the row, column and text needed to be changed in the front end
     def updateFrontBoard(row, col, dir, word):
         frontList = []
         if(dir.lower() = "right")
@@ -27,3 +41,9 @@ class endTurn:
                 frontList.append(configTuple)
                 countRow ++
         return frontList
+    
+    def calculateScore():
+    
+    ## @brief 
+    def checkWinState():
+        return Rack.getRackLength == 0 && Bag.getRemainingTiles == 0
