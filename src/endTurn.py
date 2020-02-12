3e128c7f24074c46efbfad630a2d3874f78d1c70
Lucia Cristiano <cristial@mcmaster.ca>
2020-02-12 15:11:50 -0500

    adding doxygen comments

--- src/endTurn.py
+++ src/endTurn.py
@@ -5,10 +5,10 @@
 class endTurn:
 
     ## @brief updates the back end version of the board with the valid word.
-    # @param1 an integer that represents the row of an the back end array.
-    # @param2 an integer that represents the column of the back end array.
-    # @param3 a string that represents the direction of the word placed on the board.
-    # @param4 a string that represents the word being placed on the board.
+    #  @param1 an integer that represents the row of an the back end array.
+    #  @param2 an integer that represents the column of the back end array.
+    #  @param3 a string that represents the direction of the word placed on the board.
+    #  @param4 a string that represents the word being placed on the board.
     def updateBackBoard(row, col, dir, word):
         if(dir.lower() = "right")
             countCol = col
@@ -21,11 +21,11 @@ class endTurn:
                 backBoard[col][countRow] = char
                 countRow ++
     ## @brief updates the back end version of the board with the valid word.
-    # @param1 an integer that represents the row of an the back end array.
-    # @param2 an integer that represents the column of the back end array.
-    # @param3 a string that represent the direction of the word placed on the board.
-    # @param4 a string that represents the word being placed on the board.
-    # @returns a list of tuples representing the row, column and text needed to be changed in the front end
+    #  @param1 an integer that represents the row of an the back end array.
+    #  @param2 an integer that represents the column of the back end array.
+    #  @param3 a string that represent the direction of the word placed on the board.
+    #  @param4 a string that represents the word being placed on the board.
+    #  @returns a list of tuples representing the row, column and text needed to be changed in the front end
     def updateFrontBoard(row, col, dir, word):
         frontList = []
         if(dir.lower() = "right")
@@ -44,6 +44,7 @@ class endTurn:
     
     def calculateScore():
     
-    ## @brief 
+    ## @brief checks if the game has been won
+    #  @return returns a bool to represent win state or not
     def checkWinState():
         return Rack.getRackLength == 0 && Bag.getRemainingTiles == 0
