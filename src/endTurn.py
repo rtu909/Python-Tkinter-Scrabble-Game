5cbceba7db4dc0d43c394436f41411e048d3abeb
Lucia Cristiano <cristial@mcmaster.ca>
2020-02-12 14:55:18 -0500

    Implementation of endTurn

---
+++ src/endTurn.py
@@ -0,0 +1,15 @@
+class endTurn:
+
+    def updateBackBoard(row, col, dir, word):
+        if(dir.lower() = "right")
+            countCol = col
+            for char in word:
+                backBoard[row][countCol] = char
+                countCol ++
+        elif(dir.lower() = "down")
+            countRow = row
+            for char in word:
+                backBoard[col][countRow] = char
+                countRow ++
+    
+    def updateFrontBoard():
