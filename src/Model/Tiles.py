13f010d4782aacbdbf11f5130f227b02fa77abb2
Raymond Tu <tur1@mcmaster.ca>
2020-03-07 14:41:30 -0500

    garbage

--- src/Model/Tiles.py
+++ src/Model/Tiles.py
@@ -3,33 +3,11 @@ class Tile:
     Class that allows for the creation of a tile. Initializes using an uppercase string of one letter,
     and an integer representing that letter's score.
     """
-    LETTER_VALUES = {"A": 1,
-                 "B": 3,
-                 "C": 3,
-                 "D": 2,
-                 "E": 1,
-                 "F": 4,
-                 "G": 2,
-                 "H": 4,
-                 "I": 1,
-                 "J": 1,
-                 "K": 5,
-                 "L": 1,
-                 "M": 3,
-                 "N": 1,
-                 "O": 1,
-                 "P": 3,
-                 "Q": 10,
-                 "R": 1,
-                 "S": 1,
-                 "T": 1,
-                 "U": 1,
-                 "V": 4,
-                 "W": 4,
-                 "X": 8,
-                 "Y": 4,
-                 "Z": 10}
+
     def __init__(self, letter):
+        LETTER_VALUES = {"A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 1, "K": 5,
+        "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8,
+        "Y": 4, "Z": 10}
         #Initializes the tile class. Takes the letter as a string, and the dictionary of letter values as arguments
         self.letter = letter.upper()
         self.score = 0
@@ -37,7 +15,7 @@ class Tile:
             self.score = LETTER_VALUES[self.letter]
         else:
             self.score = 0
-			
+
     def getLetter(self):
         #Returns the tile's letter (string).
         return self.letter
--- src/mainBackEnd.py
+++ src/mainBackEnd.py
@@ -1,26 +1,20 @@
 import sys
 sys.path.insert(0, 'C:/Users/Raymond/Documents/XA3/thetrifecta_scrabble/src/Model')
 
-#from Board import *
+from Board import *
 from Bag import *
 from Player import *
 from Rack import *
-#from Tiles import *
-#from boardChecks import *
+from Tiles import *
+from boardChecks import *
 from wordChecks import *
 from endTurn import *
 from frontEndMain import*
 
-self.mainBoard = Board()
-self.mainBag = Bag()
-self.player1 = Player(mainBag)
-self.player2 = Player(mainBag)
+mainBoard = Board()
+mainBag = Bag()
+player1 = Player(mainBag)
+player2 = Player(mainBag)
 
 
 frontEndMain()
-
-	
-	
-	
-	
-	
