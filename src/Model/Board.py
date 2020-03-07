dd1bc35f9f7aaf3d13e3c09b719ca3c4ae8510c5
Lucia Cristiano <cristial@mcmaster.ca>
2020-03-07 14:22:18 -0500

    fixing backend version of board

--- src/Model/Board.py
+++ src/Model/Board.py
@@ -1,41 +1,24 @@
-SCORE_TYPES = {"Normal": 0, "DLS": 1, "DWS": 2, "TLS": 3}
-
-#Use below lists when making tiles on window(was trying to ease your job with this).
-DLS_Tiles = [[0, 3], [0, 11], [2, 6], [2, 8], [3, 0], [3, 7], [3, 14], [6, 2], [6, 6], [6, 8], [6, 12], [7, 3], 
-			[7, 11], [8, 2], [8, 6], [8, 8], [8, 12], [11, 0], [11, 7], [11, 14], [12, 6], [12, 8], [14, 3], [14, 11]]
-			
-DWS_Tiles = [[1, 1], [1, 13], [2, 2], [2, 12], [3, 3], [3, 11], [4, 4], [4, 10], 
-			[10, 4], [10, 10], [11, 3], [11, 11], [12, 2], [12, 12], [13, 1], [13, 3]]
-			
-TLS_Tiles = [[1, 5], [1, 9], [5, 1], [5, 5], [5, 9], [5, 13], [9, 1], [9, 5], [9, 9], [9, 13], [13, 5], [13, 9]]
 
 class Board:
     def __init__(self):
-        board = [[]]
-        for row in range(0, 15):
-			colList = []
-            for col in range(0, 15):
-                colList.append(0)
-			list.append(
-        
-        self.tileBoard = board
-        self.scoreBoard = list
-        self.addPremiumSquares()
-        
-    
-    def addPremiumSquares(self):
-        #Adds all of the premium squares that influence the word's score.
-        TRIPLE_WORD_SCORE = ((0,0), (7, 0), (14,0), (0, 7), (14, 7), (0, 14), (7, 14), (14,14))
-        DOUBLE_WORD_SCORE = ((1,1), (2,2), (3,3), (4,4), (1, 13), (2, 12), (3, 11), (4, 10), (13, 1), (12, 2), (11, 3), (10, 4), (13,13), (12, 12), (11,11), (10,10))
-        TRIPLE_LETTER_SCORE = ((1,5), (1, 9), (5,1), (5,5), (5,9), (5,13), (9,1), (9,5), (9,9), (9,13), (13, 5), (13,9))
-        DOUBLE_LETTER_SCORE = ((0, 3), (0,11), (2,6), (2,8), (3,0), (3,7), (3,14), (6,2), (6,6), (6,8), (6,12), (7,3), (7,11), (8,2), (8,6), (8,8), (8, 12), (11,0), (11,7), (11,14), (12,6), (12,8), (14, 3), (14, 11))
+        self.backBoard = [["0" for col in range(0, 15)] for row in range(0, 15)]
 
-        for coordinate in TRIPLE_WORD_SCORE:
-            self.scoreBoard[coordinate[0]][coordinate[1]] = "TWS"
-        for coordinate in TRIPLE_LETTER_SCORE:
-            self.scoreBoard[coordinate[0]][coordinate[1]] = "TLS"
-        for coordinate in DOUBLE_WORD_SCORE:
-            self.scoreBoard[coordinate[0]][coordinate[1]] = "DWS"
-        for coordinate in DOUBLE_LETTER_SCORE:
-            self.scoreBoard[coordinate[0]][coordinate[1]] = "DLS"
-        
+    def getBoard(self):
+        return backBoard
+    
+    ## @brief updates the back end version of the board with the valid word.
+    #  @param1 an integer that represents the row of an the back end array.
+    #  @param2 an integer that represents the column of the back end array.
+    #  @param3 a string that represents the direction of the word placed on the board.
+    #  @param4 a string that represents the word being placed on the board.
+    def updateBackBoard(self, row, col, dir, word):
+        if(dir.lower() == "right"):
+            countCol = col
+            for char in word:
+                backBoard[row][countCol] = char
+                countCol += 1 
+        elif(dir.lower() == "down"):
+            countRow = row
+            for char in word:
+                backBoard[col][countRow] = char
+                countRow += 1
