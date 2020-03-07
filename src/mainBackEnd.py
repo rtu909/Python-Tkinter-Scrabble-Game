c6f6d84a3f2bc4c816af62d33733a0de3271cfb0
Raymond Tu <tur1@mcmaster.ca>
2020-03-07 14:33:06 -0500

    changed main

--- src/mainBackEnd.py
+++ src/mainBackEnd.py
@@ -1,15 +1,26 @@
-from Model/Board import *
-from Model/Bag import *
-from Model/Player import *
-from Model/Rack import *
-from Model/Tiles import *
-from boardChecks import *
+import sys
+sys.path.insert(0, 'C:/Users/Raymond/Documents/XA3/thetrifecta_scrabble/src/Model')
+
+#from Board import *
+from Bag import *
+from Player import *
+from Rack import *
+#from Tiles import *
+#from boardChecks import *
 from wordChecks import *
 from endTurn import *
+from frontEndMain import*
 
-class mainBackEnd:
-	mainBoard = Board.Board()
-	player1 = Player.Player()
-	player2 = Player.Player()
-	mainBag = Bag.Bag()
+self.mainBoard = Board()
+self.mainBag = Bag()
+self.player1 = Player(mainBag)
+self.player2 = Player(mainBag)
+
+
+frontEndMain()
+
+	
+	
+	
+	
 	
